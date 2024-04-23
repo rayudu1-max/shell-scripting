09253f6eca643a946aed03b6dd93d8207f04fb28
rayudu1-max <subbuhari999@gmail.com>
2024-04-23 13:00:31 +0530

    One go

--- 04-conditions.sh
+++ 04-conditions.sh
@@ -7,6 +7,8 @@ read NUMBER
 if [ $NUMBER -gt 15 ]
 then
     echo "$NUMBER is greater than 15"
+else if [ $NUMBER -eq 15]
+    echo "$NUMBER is equal to 15"
 else
     echo "$NUMBER is lesser than 15"
 fi
