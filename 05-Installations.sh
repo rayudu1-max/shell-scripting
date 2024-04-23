ed64a003b93d4ec0749eb1e6ff58fccc8b337273
rayudu1-max <subbuhari999@gmail.com>
2024-04-23 13:25:53 +0530

    One go

---
+++ 05-Installations.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [$USERID -ne 0]
+then
+    echo "run the script with root access"
+    exit 1
+else 
+    echo "you are a super user"
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0]
+then 
+    echo "installation status of MYSQL....FAILURE"
+    exit 1
+else
+    echo "installation status of MYSQL....SUCCESS"
+fi
+
+dnf install git -y
+
+if [ $? -ne 0]
+then
+    echo "installation status of git....FAILURE"
+    exit 1
+else
+    echo "installation status of git....SUCCESS"
+fi
+
+echo "script reached its end"
