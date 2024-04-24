3d10af5e621ac9055df3c06c34aa6659fa90b57d
rayudu1-max <subbuhari999@gmail.com>
2024-04-24 13:51:49 +0530

    Shell scripting pactice

--- 07-Loops.sh
+++ 07-Loops.sh
@@ -10,12 +10,12 @@ Y="\e[33m"
 N="\e[0m"
 
 VALIDATE () {
-if [ $1 -eq 0 ]
+if [ $1 -ne 0 ]
 then
-    echo -e "$2 ...$G SUCCESS $N"
+    echo -e "$2 ...$R FAILURE $N"
     exit 1
 else
-    echo -e "$2 ...$R FAILED $N"
+    echo -e "$2 ...$G FAILED $N"
 fi
 }
 
